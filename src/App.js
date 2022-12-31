91ad22c0075174e5af547349ba6699b600250f87
ZdravkoBarukchiev <zpb@abv.bg>
2022-12-31 13:31:47 +0200

    create games

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Games } from './components/games/Games';
 import { Home } from './components/homePage/Home';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Games/>
     </div>
   );
 }
---
+++ src/components/games/Games.js
@@ -0,0 +1,78 @@
+export const Games=()=>{
+    return(
+        <div id="game" className="casino">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="titlepage">
+                <h2>Our Casino Games</h2>
+                <span />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 padding_bottom">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game1.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 1</h3>
+              </div>
+            </div>
+            <div className="col-md-4 padding_bottom">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game2.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 2</h3>
+              </div>
+            </div>
+            <div className="col-md-4 padding_bottom">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game3.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 3</h3>
+              </div>
+            </div>
+            <div className="col-md-4 margin_bottom1">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game4.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 4</h3>
+              </div>
+            </div>
+            <div className="col-md-4 margin_bottom1">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game5.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 5</h3>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="game_box">
+                <figure>
+                  <img src="images/game6.jpg" alt="#" />
+                </figure>
+              </div>
+              <div className="game">
+                <h3>Game 6</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
