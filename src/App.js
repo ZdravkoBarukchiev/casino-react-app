255bbc16bfebbe5112684409ee0e07e12fa0aab0
ZdravkoBarukchiev <zpb@abv.bg>
2022-12-31 15:50:21 +0200

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { Customer } from "./components/customer/Customer";
 import { Footer } from "./components/footer/Footer";
 import { Games } from "./components/games/Games";
@@ -8,12 +9,14 @@ import { Reqeste } from "./components/reqeste/Reqeste";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Games />
-      <Licens />
-      <Customer />
-      <Reqeste />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/licens" element={<Licens />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/reqeste" element={<Reqeste />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
     </div>
   );
 }
