a71410c73463418f161605deaf73a034fdd25f5d
ZdravkoBarukchiev <zpb@abv.bg>
2023-01-01 19:13:18 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { Customer } from "./components/customer/Customer";
-import { Footer } from "./components/footer/Footer";
 import { Games } from "./components/games/Games";
 import { Home } from "./components/homePage/Home";
 import { Licens } from "./components/licens/Licens";
@@ -16,7 +15,6 @@ function App() {
         <Route path="/licens" element={<Licens />} />
         <Route path="/customer" element={<Customer />} />
         <Route path="/reqeste" element={<Reqeste />} />
-        <Route path="/footer" element={<Footer />} />
         <Route path="/promotins" element={<Promotins />} />
       </Routes>
     </div>
