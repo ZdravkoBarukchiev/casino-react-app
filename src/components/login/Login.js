77909c13cfbe7dd2a4a8bde2a8dbb8c2cdf63ec7
ZdravkoBarukchiev <zpb@abv.bg>
2023-01-04 18:57:28 +0200

    updated login and register

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -61,9 +61,9 @@ export const Login = () => {
                   <div className="col-md-12">
                     <input
                       className="form_control"
-                      placeholder="Username"
+                      placeholder="Email"
                       type="text"
-                      name="Username"
+                      name="Email"
                     />
                   </div>
                   <div className="col-md-12">
@@ -83,6 +83,75 @@ export const Login = () => {
           </div>
         </div>
       </div>
+      <footer>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 ">
+                <div className="address">
+                  <h3>Subscribe Now</h3>
+                  <form className="newtetter">
+                    <button className="submit">submit</button>
+                    <input
+                      className="tetter"
+                      placeholder="Enter your email"
+                      type="text"
+                      name="Enter your email"
+                    />
+                  </form>
+                </div>
+              </div>
+              <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div className="row">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <div className="address">
+                      <h3>Links</h3>
+                      <ul className="Menu_footer">
+                        <li className="active">
+                          {" "}
+                          <Link to="/">Home</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/promotins">Promotins</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/games">Games</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/customers">Customers</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/licens">Licens</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <div className="address">
+                      <h3>Contact us</h3>
+                      <ul className="Links_footer">
+                        <li>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do{" "}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="copyright">
+            <div className="container">
+              <p>
+                Copyright 2019 All Right Reserved By{" "}
+                <a href="https://html.design/">Free html Templates</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="overlay" />
     </div>
   );
 };
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -61,22 +61,6 @@ export const Register = () => {
                   <div className="col-md-12">
                     <input
                       className="form_control"
-                      placeholder="Username"
-                      type="text"
-                      name="Username"
-                    />
-                  </div>
-                  <div className="col-md-12">
-                    <input
-                      className="form_control"
-                      placeholder="Phone number"
-                      type="text"
-                      name="Phone number"
-                    />
-                  </div>
-                  <div className="col-md-12">
-                    <input
-                      className="form_control"
                       placeholder="Email"
                       type="text"
                       name="Email"
@@ -107,6 +91,75 @@ export const Register = () => {
           </div>
         </div>
       </div>
+      <footer>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 ">
+                <div className="address">
+                  <h3>Subscribe Now</h3>
+                  <form className="newtetter">
+                    <button className="submit">submit</button>
+                    <input
+                      className="tetter"
+                      placeholder="Enter your email"
+                      type="text"
+                      name="Enter your email"
+                    />
+                  </form>
+                </div>
+              </div>
+              <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div className="row">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <div className="address">
+                      <h3>Links</h3>
+                      <ul className="Menu_footer">
+                        <li className="active">
+                          {" "}
+                          <Link to="/">Home</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/promotins">Promotins</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/games">Games</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/customers">Customers</Link>{" "}
+                        </li>
+                        <li>
+                          <Link to="/licens">Licens</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <div className="address">
+                      <h3>Contact us</h3>
+                      <ul className="Links_footer">
+                        <li>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do{" "}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="copyright">
+            <div className="container">
+              <p>
+                Copyright 2019 All Right Reserved By{" "}
+                <a href="https://html.design/">Free html Templates</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="overlay" />
     </div>
   );
 };
