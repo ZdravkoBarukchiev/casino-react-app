72b4919aa8a18d4625212bba121b188811db973c
ZdravkoBarukchiev <zpb@abv.bg>
2023-01-08 16:32:34 +0200

    updated

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -1,16 +1,16 @@
 import { Navigation } from "../navigation/Navigation";
 import { Footer } from "../footer/Footer";
 import { Game } from "./game/Game";
+import { useState, useEffect } from "react";
 
 export const Games = () => {
-  const allGames = [
-    { name: "Game 1", image: "images/game1.jpg", id: 1 },
-    { name: "Game 2", image: "images/game2.jpg", id: 2 },
-    { name: "Game 3", image: "images/game3.jpg", id: 3 },
-    { name: "Game 4", image: "images/game4.jpg", id: 4 },
-    { name: "Game 5", image: "images/game5.jpg", id: 5 },
-    { name: "Game 6", image: "images/game6.jpg", id: 6 },
-  ];
+  const [games, setGame] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3030/data/games")
+      .then((res) => res.json())
+      .then((result) => setGame(result));
+  }, []);
+  console.log(games);
   return (
     <>
       <Navigation />
@@ -25,8 +25,8 @@ export const Games = () => {
             </div>
           </div>
           <div className="row">
-            {allGames.map((g) => (
-              <Game key={g.id} name={g.name} image={g.image} />
+            {games.map((g) => (
+              <Game key={g._id} name={g.title} image={g.imageUrl} />
             ))}
           </div>
         </div>
